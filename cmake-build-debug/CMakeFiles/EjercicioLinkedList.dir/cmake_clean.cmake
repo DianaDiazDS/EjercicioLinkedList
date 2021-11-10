c4b9b6a2fc4e56da590b8d73cc1111644a19d4a7
DianaDiazDS <diana.diaz15@uptc.edu.co>
2021-11-10 11:30:20 -0500

    cambiso

---
+++ cmake-build-debug/CMakeFiles/EjercicioLinkedList.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/EjercicioLinkedList.dir/Fruta.cpp.obj"
+  "CMakeFiles/EjercicioLinkedList.dir/LinkedList.cpp.obj"
+  "CMakeFiles/EjercicioLinkedList.dir/Node.cpp.obj"
+  "CMakeFiles/EjercicioLinkedList.dir/main.cpp.obj"
+  "EjercicioLinkedList.exe"
+  "EjercicioLinkedList.exe.manifest"
+  "EjercicioLinkedList.pdb"
+  "libEjercicioLinkedList.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/EjercicioLinkedList.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
