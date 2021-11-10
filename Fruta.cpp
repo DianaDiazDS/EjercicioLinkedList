c3ff54f660d50db6fafef0d2c82d1a6e7c50aeb5
DianaDiazDS <diana.diaz15@uptc.edu.co>
2021-11-10 12:20:40 -0500

    se agrego

--- Fruta.cpp
+++ Fruta.cpp
@@ -3,3 +3,39 @@
 //
 
 #include "Fruta.h"
+
+Fruta::Fruta() {}
+
+Fruta::Fruta(const std::string &name, const std::string &color, const std::string &sabor) : name(name), color(color),
+                                                                                            sabor(sabor) {}
+
+
+
+const std::string &Fruta::getName() const {
+    return name;
+}
+
+void Fruta::setName(const std::string &name) {
+    Fruta::name = name;
+}
+
+const std::string &Fruta::getColor() const {
+    return color;
+}
+
+void Fruta::setColor(const std::string &color) {
+    Fruta::color = color;
+}
+
+const std::string &Fruta::getSabor() const {
+    return sabor;
+}
+
+void Fruta::setSabor(const std::string &sabor) {
+    Fruta::sabor = sabor;
+}
+
+std::ostream &operator<<(std::ostream &os, const Fruta &fruta) {
+    os << "name: " << fruta.name << " color: " << fruta.color << " sabor: " << fruta.sabor;
+    return os;
+}
--- Fruta.h
+++ Fruta.h
@@ -5,8 +5,41 @@
 #ifndef EJERCICIOLINKEDLIST_FRUTA_H
 #define EJERCICIOLINKEDLIST_FRUTA_H
 
+#include <string>
+#include <ostream>
+
 
 class Fruta {
+
+public:
+    Fruta();
+
+    Fruta(const std::string &name, const std::string &color, const std::string &sabor);
+
+    const std::string &getName() const;
+
+    void setName(const std::string &name);
+
+    const std::string &getColor() const;
+
+    void setColor(const std::string &color);
+
+    const std::string &getSabor() const;
+
+    void setSabor(const std::string &sabor);
+
+
+    friend std::ostream &operator<<(std::ostream &os, const Fruta &fruta);
+
+private:
+
+    std::string name;
+    std::string color;
+    std::string sabor;
+
+
+
+
 
 };
 
